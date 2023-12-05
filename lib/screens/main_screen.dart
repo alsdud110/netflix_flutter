4fe5da3582cfd29109635e818f0b72acd0c074f2
황민영 <myhwang@hwangmin-yeongs-MacBook-Pro.local>
2023-12-05 21:23:46 +0900

    MainScreen 구현 다시 한번 bottomNavigationBar 복습

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:netflix_flutter/constants.dart';
+import 'package:netflix_flutter/models/nav_item.dart';
+import 'package:netflix_flutter/screens/coming/coming_screen.dart';
+import 'package:netflix_flutter/screens/contents_list/content_list_screen.dart';
+import 'package:netflix_flutter/screens/home/home_screen.dart';
 
 class MainScreen extends StatefulWidget {
   static String routeName = "/main_screen";
@@ -9,10 +14,34 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("MainScreen")),
+    return Scaffold(
+      body: const IndexedStack(
+        children: [
+          HomeScreen(),
+          ComingScreen(),
+          ContentsListScreen(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.shifting,
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: kDarkColor,
+        selectedFontSize: 12.0,
+        onTap: (value) => setState(() {
+          _selectedIndex = value;
+        }),
+        items: List.generate(
+          navItems.length,
+          (index) => BottomNavigationBarItem(
+            icon: Icon(navItems[index].icon),
+            label: navItems[index].label,
+          ),
+        ),
+      ),
     );
   }
 }
