d0cdcfc7993266817602890bec0f0b4cc2dcbcfc
황민영 <myhwang@hwangmin-yeongs-MacBook-Pro.local>
2023-12-08 22:29:32 +0900

    동시에 움직이는 두개의 스크롤뷰

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -16,6 +16,8 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    ScrollController backController = ScrollController();
+    ScrollController frontController = ScrollController();
     List<String> posters = [
       "assets/images/big_buck_bunny_poster.jpg",
       "assets/images/les_miserables_poster.jpg",
@@ -23,185 +25,238 @@ class _HomeScreenState extends State<HomeScreen> {
       "assets/images/the_book_of_fish_poster.jpg"
     ];
     Size appSize = MediaQuery.of(context).size;
-    return SafeArea(
-      child: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            leading: const Center(
-              child: Text(
-                "M",
-                style: TextStyle(
-                  color: Colors.red,
-                  fontSize: 26,
-                  fontWeight: FontWeight.bold,
-                ),
+    return Stack(
+      children: [
+        SingleChildScrollView(
+          controller: backController,
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  Image(
+                    image: AssetImage(posters[0]),
+                    height: appSize.height * 0.6 +
+                        (const SliverAppBar().toolbarHeight * 2),
+                    width: double.infinity,
+                    fit: BoxFit.fitWidth,
+                  ),
+                  Container(
+                    height: appSize.height * 0.6 +
+                        (const SliverAppBar().toolbarHeight * 2),
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Colors.black.withOpacity(0.2),
+                          Colors.black.withOpacity(0.0),
+                          Colors.black,
+                        ],
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        stops: const [0.0, 0.5, 0.9],
+                      ),
+                    ),
+                  )
+                ],
               ),
-            ),
-            actions: [
-              IconButton(
-                icon: const Icon(FontAwesomeIcons.chromecast),
-                onPressed: () {},
-              ),
-              IconButton(
-                icon: const Icon(FontAwesomeIcons.search),
-                onPressed: () {},
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 14),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(5),
-                  child:
-                      const Image(image: AssetImage("assets/images/dog.jpg")),
-                ),
-              ),
-              const SizedBox(
-                width: 10,
-              ),
+              SizedBox(height: appSize.height),
             ],
           ),
-          const SliverAppBar(
-            pinned: true, // AppBar가 하단 List 내렸을 때 바로 보여야 한다 -> true
-            centerTitle: true,
-            automaticallyImplyLeading: false,
-            //하단 List를 최상단으로 올렸을때의 SliverAppBar의 Default height
-            //expandedHeight를 사용하면 스크롤을 내리면 toolbarheight높이까지 줄어든다.
-            // expandedHeight: 100.0,
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Text("TV 프로그램"),
-                Text("영화"),
-                Text("내가 찜한 콘텐츠"),
+        ),
+        SafeArea(
+          child: NotificationListener(
+            onNotification: (ScrollNotification scrollInfo) {
+              if (frontController.offset <= appSize.height) {
+                backController.jumpTo(frontController.offset);
+                return true;
+              } else {
+                return false;
+              }
+            },
+            child: CustomScrollView(
+              controller: frontController,
+              slivers: [
+                SliverAppBar(
+                  backgroundColor: Colors.transparent,
+                  leading: const Center(
+                    child: Text(
+                      "M",
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontSize: 26,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  actions: [
+                    IconButton(
+                      icon: const Icon(FontAwesomeIcons.chromecast),
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: const Icon(FontAwesomeIcons.search),
+                      onPressed: () {},
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 14),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(5),
+                        child: const Image(
+                            image: AssetImage("assets/images/dog.jpg")),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 15,
+                    ),
+                  ],
+                ),
+                const SliverAppBar(
+                  pinned: true, // AppBar가 하단 List 내렸을 때 바로 보여야 한다 -> true
+                  centerTitle: true,
+                  automaticallyImplyLeading: false,
+                  backgroundColor: Colors.transparent,
+                  shadowColor: Colors.transparent,
+                  //하단 List를 최상단으로 올렸을때의 SliverAppBar의 Default height
+                  //expandedHeight를 사용하면 스크롤을 내리면 toolbarheight높이까지 줄어든다.
+                  // expandedHeight: 100.0,
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text("TV 프로그램"),
+                      Text("영화"),
+                      Text("내가 찜한 콘텐츠"),
+                    ],
+                  ),
+                ),
+                SliverToBoxAdapter(
+                  child: SizedBox(
+                    height: (appSize.height * 0.6),
+                    child: const Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          "오늘 한국에서 콘텐스 순위 1위",
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            LabelIcon(icon: Icons.add, label: "내가 찜한 콘텐츠"),
+                            PlayButton(
+                              width: 80,
+                            ),
+                            LabelIcon(
+                              icon: Icons.info_outline,
+                              label: "정보",
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SliverPadding(
+                  padding: const EdgeInsets.only(bottom: 40, left: 10),
+                  sliver: SliverToBoxAdapter(
+                    child: SizedBox(
+                      height: 200,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          RichText(
+                            text: const TextSpan(
+                                text: "오늘 한국의 ",
+                                children: [
+                                  TextSpan(
+                                    text: "TOP 10",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  TextSpan(text: " 콘텐스"),
+                                ],
+                                style: TextStyle(fontSize: 18)),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Expanded(
+                            child: ListView(
+                              scrollDirection: Axis.horizontal,
+                              children: List.generate(
+                                posters.length,
+                                (index) => SizedBox(
+                                  width: 150,
+                                  height: 200,
+                                  child: RankPoster(
+                                    rank: (index + 1).toString(),
+                                    posterUrl: posters[index],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SliverPadding(
+                  padding: const EdgeInsets.only(bottom: 40, left: 10),
+                  sliver: SliverToBoxAdapter(
+                    child: SizedBox(
+                      height: 200,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          RichText(
+                            text: const TextSpan(
+                              children: [
+                                TextSpan(
+                                  text: "TV ",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                TextSpan(text: "프로그램 · 로맨스"),
+                              ],
+                              style: TextStyle(
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Expanded(
+                            child: ListView(
+                              scrollDirection: Axis.horizontal,
+                              children: List.generate(
+                                posters.length,
+                                (index) => Poster(
+                                  posterUrl: posters[index],
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
-          SliverToBoxAdapter(
-            child: SizedBox(
-              height: (appSize.height * 0.6),
-              child: const Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text(
-                    "오늘 한국에서 콘텐스 순위 1위",
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      LabelIcon(icon: Icons.add, label: "내가 찜한 콘텐츠"),
-                      PlayButton(
-                        width: 80,
-                      ),
-                      LabelIcon(
-                        icon: Icons.info_outline,
-                        label: "정보",
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                ],
-              ),
-            ),
-          ),
-          SliverPadding(
-            padding: const EdgeInsets.only(bottom: 40, left: 10),
-            sliver: SliverToBoxAdapter(
-              child: SizedBox(
-                height: 200,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    RichText(
-                      text: const TextSpan(
-                          text: "오늘 한국의 ",
-                          children: [
-                            TextSpan(
-                              text: "TOP 10",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            TextSpan(text: " 콘텐스"),
-                          ],
-                          style: TextStyle(fontSize: 18)),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Expanded(
-                      child: ListView(
-                        scrollDirection: Axis.horizontal,
-                        children: List.generate(
-                          posters.length,
-                          (index) => SizedBox(
-                            width: 150,
-                            height: 200,
-                            child: RankPoster(
-                              rank: (index + 1).toString(),
-                              posterUrl: posters[index],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          SliverPadding(
-            padding: const EdgeInsets.only(bottom: 40, left: 10),
-            sliver: SliverToBoxAdapter(
-              child: SizedBox(
-                height: 200,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    RichText(
-                      text: const TextSpan(
-                        children: [
-                          TextSpan(
-                            text: "TV ",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          TextSpan(text: "프로그램 - 로맨스"),
-                        ],
-                        style: TextStyle(
-                          fontSize: 18,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Expanded(
-                      child: ListView(
-                        scrollDirection: Axis.horizontal,
-                        children: List.generate(
-                          posters.length,
-                          (index) => Poster(
-                            posterUrl: posters[index],
-                          ),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
